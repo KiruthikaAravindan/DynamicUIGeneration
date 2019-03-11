5d48cc732b6bf3b340846f0600a4d44258cbc7f9
sudarshanmeetyou <sudarshanmeetyou@gmail.com>
2019-03-11 16:50:17 +0530

    Added a comment just to test github desktop

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -12,7 +12,7 @@ sap.ui.define([
 			oDetailModel.refresh();
 			this.createDynamic();
 		},
-
+// Function to create dynamically
 		createDynamic: function () {
 			var oPage = this.getView().byId("idPage");
 			oPage.removeAllContent();
